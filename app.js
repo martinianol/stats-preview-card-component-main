636b5711a9b15a402e2277e61e61867510da25cd
Mars <martinianol@outlook.com>
2021-05-20 23:26:35 -0300

    Todo listo para empezar a trabajar

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.listen(3000,() => console.log('Servidor corriendo en el puerto 3000'));
 
 app.get('/',(req,res) => {
     
-    res.sendFile(path.resolve(__dirname,'./views/home.html'))
+    res.sendFile(path.resolve(__dirname,'./views/index.html'))
 })
